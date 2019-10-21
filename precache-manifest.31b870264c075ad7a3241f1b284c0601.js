7c534a3e229b9af0627c38f29a1959d03c16a01a
neltsker <neltsker@gmail.com>
2019-10-21 20:56:19 +0300

    Updates

---
+++ precache-manifest.31b870264c075ad7a3241f1b284c0601.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b49bae088c05da0a7fbbe61dea6640",
+    "url": "/vk-calc-app1/index.html"
+  },
+  {
+    "revision": "6a10475ad878e5b13239",
+    "url": "/vk-calc-app1/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "4e5b1f8263cd316e9fa0",
+    "url": "/vk-calc-app1/static/css/main.e04366c9.chunk.css"
+  },
+  {
+    "revision": "6a10475ad878e5b13239",
+    "url": "/vk-calc-app1/static/js/2.bbfea624.chunk.js"
+  },
+  {
+    "revision": "4e5b1f8263cd316e9fa0",
+    "url": "/vk-calc-app1/static/js/main.e13ce490.chunk.js"
+  },
+  {
+    "revision": "ae74bbb5ed751ef068f7",
+    "url": "/vk-calc-app1/static/js/runtime-main.a4da31ab.js"
+  },
+  {
+    "revision": "0ce050208fe3b390e475bc5227ba92a7",
+    "url": "/vk-calc-app1/static/media/01.0ce05020.jpg"
+  },
+  {
+    "revision": "7d712c608b88e6201bf6ad033f771812",
+    "url": "/vk-calc-app1/static/media/01.7d712c60.png"
+  },
+  {
+    "revision": "8ace344ee4c19234fb3f2759e1e8769d",
+    "url": "/vk-calc-app1/static/media/01.8ace344e.jpg"
+  },
+  {
+    "revision": "5901da7a1a4e137d72bcb047ad521642",
+    "url": "/vk-calc-app1/static/media/02.5901da7a.jpg"
+  },
+  {
+    "revision": "92419a213a33b30249eb8504705cdcdf",
+    "url": "/vk-calc-app1/static/media/02.92419a21.jpg"
+  },
+  {
+    "revision": "d96d31639c1dc359c5831558f995f5c6",
+    "url": "/vk-calc-app1/static/media/02.d96d3163.jpg"
+  },
+  {
+    "revision": "e7aa522622dc76a480880b6d40b9b994",
+    "url": "/vk-calc-app1/static/media/02.e7aa5226.png"
+  },
+  {
+    "revision": "10a5883af227710591d81eb88daee1d2",
+    "url": "/vk-calc-app1/static/media/03.10a5883a.png"
+  },
+  {
+    "revision": "492e2ef38bd68c1cb9ca2d32471f7489",
+    "url": "/vk-calc-app1/static/media/03.492e2ef3.jpg"
+  },
+  {
+    "revision": "67621612b8e998d121e12068d3795f91",
+    "url": "/vk-calc-app1/static/media/03.67621612.jpg"
+  },
+  {
+    "revision": "cf779a17a24ef30147e82a231c2ece7f",
+    "url": "/vk-calc-app1/static/media/03.cf779a17.jpg"
+  },
+  {
+    "revision": "9d610436b1e76f8dac364b015411cad9",
+    "url": "/vk-calc-app1/static/media/04.9d610436.png"
+  },
+  {
+    "revision": "a1525a57a1b44880a5ff43ad63a68ab7",
+    "url": "/vk-calc-app1/static/media/04.a1525a57.jpg"
+  },
+  {
+    "revision": "e7d7ce1751a0cb8fd52cbe1d8022f799",
+    "url": "/vk-calc-app1/static/media/05.e7d7ce17.jpg"
+  }
+]);
